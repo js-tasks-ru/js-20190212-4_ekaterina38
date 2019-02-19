10d8075c3bae755d7871fd0068e2b5284e93272d
katyaga <aeva_ev@mail.ru>
2019-02-19 11:02:02 +0700

    Занятие 1: задача 1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,4 @@
+'use strict';
 /**
  * Power
  * @param {number} m base
@@ -5,5 +6,17 @@
  * @returns {number}
  */
 function pow (m, n) {
-
+  let result = 1;
+  if (n > 0) {
+    for (let i = 1; i <= n; i++) {
+      result = result * m;
+    }
+    return result;
+  } else {
+    alert('Вы ввели неверный показатель степени');
+  }
 }
+
+let a = +prompt('Введите основание степени: ', '0');
+let b = +prompt('Введите показатель степени (натуральное число): ', '0');
+console.log(pow(a, b));

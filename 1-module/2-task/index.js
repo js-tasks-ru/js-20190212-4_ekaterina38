2e10a59c7bd5c04245681904b33f6200def84138
katyaga <aeva_ev@mail.ru>
2019-02-19 17:13:36 +0700

    Занятие 1: задача 2, 3

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -3,26 +3,26 @@
 /**
  * Эту функцию трогать не нужно
  */
-function print (text) {
-    alert(text);
+function print(text) {
+  alert(text);
 }
 
 /**
  * Эту функцию нужно поменять так,
  * чтобы функция sayHello работала корректно
  */
-function isValid (name) {
+function isValid(name) {
+  return name !== '' && name.indexOf(' ') === -1 && name.length >= 4;
 }
 
-function sayHello () {
-    let userName = prompt('Введите ваше имя');
+function sayHello() {
+  let userName = prompt('Введите ваше имя');
 
-    if (isValid(userName)) {
-        print('Welcome back, ' + userName + '!');
-    } else {
-        print('Некорректное имя');
-    }
+  if (isValid(userName)) {
+    print('Welcome back, ' + userName + '!');
+  } else {
+    print('Некорректное имя');
+  }
 }
 
-sayHello();
-
+// sayHello();

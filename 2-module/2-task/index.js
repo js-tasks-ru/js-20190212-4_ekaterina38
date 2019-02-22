f3976247846b864b65abd4416e1972ae082b9a74
user <aevakatya@gmail.com>
2019-02-22 16:24:59 +0700

    Занятие 2: задача 1, 2

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -5,4 +5,50 @@
  * @returns {Object}
  */
 function find (obj, value) {
+    let onePath = '';
+    let path = [];
+    let numberKeys = 0;
+
+    for (let key in obj) {
+        if (obj[key] === value) {
+            onePath = key + '.';
+            onePath = onePath.slice(0, length - 1);
+            path.push(onePath);
+            numberKeys++;
+        }
+
+        if (typeof obj[key] === 'object' && obj[key] !== null) {
+            //onePath = key + '.' + find (obj[key], value);
+            if (find (obj[key], value) !== null) {
+                onePath = key + '.' + find (obj[key], value);
+                path.push(onePath);
+            }
+        }
+    }
+
+    if (path.length > 0) {
+        if (path.length === 1) {
+            path = path.join('');
+        }
+        return path;
+    }
+    return null;
 }
+
+// let obj = {
+//     style: {
+//         border: {
+//             color: 'red',
+//             prop: {
+//                 color: 'red'
+//             },
+//
+//         }
+//     }
+// };
+//
+// console.log(find(obj, 'red'));
+
+
+
+
